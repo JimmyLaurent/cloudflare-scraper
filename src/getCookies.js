4c442dd17118b6e7e430412296e36c7224820386
Jimmy Laurent <>
2020-06-06 17:29:48 +0200

    Headless true

--- src/getCookies.js
+++ src/getCookies.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 async function getCookies(url) {
   let options = {
-    headless: false,
+    headless: true,
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
