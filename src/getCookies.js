7815f5e9dd28703fcf301b4c3e2e4776115c6f12
Jimmy Laurent <>
2020-06-07 15:36:47 +0200

    Minor changes

--- src/getCookies.js
+++ src/getCookies.js
@@ -2,9 +2,11 @@ const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 
+const { PUPPETEER_HEADLESS = 'true' } = process.env;
+
 async function getCookies(url) {
   let options = {
-    headless: true,
+    headless: PUPPETEER_HEADLESS === 'true',
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
@@ -13,31 +15,36 @@ async function getCookies(url) {
   };
 
   const browser = await puppeteer.launch(options);
-	const page = await browser.newPage();
-	
-  let response = await page.goto(url, {
-    timeout: 45000,
-    waitUntil: 'domcontentloaded'
-  });
-  let count = 0;
-  let title = await page.title();
-  while (title === 'Just a moment...') {
-    response = await page.waitForNavigation({
+  try {
+    const page = await browser.newPage();
+
+    let response = await page.goto(url, {
       timeout: 45000,
       waitUntil: 'domcontentloaded'
     });
-    title = await page.title();
-    count++;
-    if (count === 10) {
-      throw new Error('timeout on just a moment');
+    let count = 0;
+    let content = await page.content();
+    while (content.includes("cf-browser-verification")) {
+      response = await page.waitForNavigation({
+        timeout: 45000,
+        waitUntil: 'domcontentloaded'
+      });
+      content = await page.content();
+      count++;
+      if (count === 10) {
+        throw new Error('timeout on just a moment');
+      }
+		}
+		const title = await page.title();
+    if (title === 'Attention Required! | Cloudflare') {
+      throw new Error('Captcha error');
     }
+    const cookies = await page.cookies();
+
+    return cookies;
+  } finally {
+    await browser.close();
   }
-  if (title === 'Attention Required! | Cloudflare') {
-    throw new Error('Captcha error');
-  }
-  const cookies = await page.cookies();
-  await browser.close();
-  return cookies;
 }
 
 module.exports = getCookies;
